bbc769fd59cfe630c98cd9335dbe9c79b24fd21e
Chanwoong Kim <ykdy3951@naver.com>
2021-03-21 00:24:53 +0900

    boj 2954

---
+++ 2954.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define endl '\n'
+#define ll long long
+#define INF 987654321
+#define sc1(a) scanf("%c", &a)
+#define ss1(a) scanf("%s", s)
+#define sd1(a) scanf("%d", &a)
+#define sd2(a, b) scanf("%d %d", &a, &b)
+#define sld1(a) scanf("%ld", &a)
+#define sld2(a, b) scanf("%ld %ld", &a, &b)
+#define slld1(a) scanf("%lld", &a)
+#define slld2(a, b) scanf("%lld %lld", &a, &b)
+#define pd1(a) printf("%d", a)
+#define pnd1(a) printf("%d\n", a)
+#define pd2(a, b) printf("%d %d", a, b)
+#define pnd2(a, b) printf("%d %d\n", a, b)
+#define st(v) sort(v.begin(), v.end())
+#define pr pair
+#define pb push_back
+#define f(i, a, b) for (i = a; i < b; i++)
+
+string s;
+
+void init()
+{
+    getline(cin, s);
+}
+
+void solve()
+{
+    int i;
+    string ans;
+    int j = 0;
+    int size = s.length();
+    f(i, 0, size)
+    {
+        bool chk = false;
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+        {
+            chk = true;
+        }
+        if (chk)
+        {
+            s = s.substr(0, i + 1) + s.substr(i + 3);
+            size -= 2;
+        }
+    }
+
+    cout << s << endl;
+}
+
+int main(void)
+{
+    fio;
+    init();
+    solve();
+    return 0;
+}
