945f1f721585ca8b584d8639759be7906312a6c9
Chanwoong Kim <ykdy3951@naver.com>
2021-03-25 02:07:38 +0900

    boj 1463

--- 1463.cpp
+++ 1463.cpp
@@ -2,41 +2,60 @@
 
 using namespace std;
 
-int num;
-priority_queue<pair<int, int>> pq;
+#define fio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define endl '\n'
+#define ll long long
+#define INF 987654321
+#define sc1(a) scanf("%c", &a)
+#define ss1(a) scanf("%s", s)
+#define sd1(a) scanf("%d", &a)
+#define sd2(a, b) scanf("%d %d", &a, &b)
+#define sd3(a, b, c) scanf("%d %d %d", &a, &b, &c)
+#define sd4(a, b, c, d) scanf("%d %d %d %d", &a, &b, &c, &d)
+#define sld1(a) scanf("%ld", &a)
+#define sld2(a, b) scanf("%ld %ld", &a, &b)
+#define slld1(a) scanf("%lld", &a)
+#define slld2(a, b) scanf("%lld %lld", &a, &b)
+#define pd1(a) printf("%d", a)
+#define psd1(a) printf("%d ", a)
+#define pnd1(a) printf("%d\n", a)
+#define pd2(a, b) printf("%d %d", a, b)
+#define pnd2(a, b) printf("%d %d\n", a, b)
+#define st(v) sort(v.begin(), v.end())
+#define pr pair
+#define pb push_back
+#define f(i, a, b) for (i = a; i < b; i++)
+
+int n;
+int dp[1000001];
 
 void init()
 {
-    scanf("%d", &num);
+    sd1(n);
 }
 
-int SOLVE()
+void solve()
 {
-    pq.push({0, num});
-    while (!pq.empty())
-    {
-        int w = -pq.top().first, num = pq.top().second;
-        pq.pop();
-        if (num == 1)
-        {
-            return w;
-        }
+    int i;
 
-        w = -(w + 1);
-        if (!(num % 3))
+    f(i, 2, n + 1)
+    {
+        dp[i] = dp[i - 1] + 1;
+        if (i % 2 == 0)
         {
-            pq.push({w, num / 3});
+            dp[i] = min(dp[i], dp[i / 2] + 1);
         }
-        if (!(num % 2))
+        if (i % 3 == 0)
         {
-            pq.push({w, num / 2});
+            dp[i] = min(dp[i], dp[i / 3] + 1);
         }
-        pq.push({w, num - 1});
     }
 }
 
 int main(void)
 {
     init();
-    printf("%d\n", SOLVE());
+    solve();
+    pnd1(dp[n]);
+    return 0;
 }
