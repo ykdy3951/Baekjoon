84fcf6d2cc63d5357fb7809f8eb29985ed966992
Chanwoong Kim <ykdy3951@naver.com>
2021-03-26 04:17:50 +0900

    boj 1475

---
+++ 1475.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define endl '\n'
+#define ll long long
+#define INF 987654321
+#define sc1(a) scanf("%c", &a)
+#define ss1(a) scanf("%s", s)
+#define sd1(a) scanf("%d", &a)
+#define sd2(a, b) scanf("%d %d", &a, &b)
+#define sd3(a, b, c) scanf("%d %d %d", &a, &b, &c)
+#define sd4(a, b, c, d) scanf("%d %d %d %d", &a, &b, &c, &d)
+#define sld1(a) scanf("%ld", &a)
+#define sld2(a, b) scanf("%ld %ld", &a, &b)
+#define slld1(a) scanf("%lld", &a)
+#define slld2(a, b) scanf("%lld %lld", &a, &b)
+#define pd1(a) printf("%d", a)
+#define psd1(a) printf("%d ", a)
+#define pnd1(a) printf("%d\n", a)
+#define pd2(a, b) printf("%d %d", a, b)
+#define pnd2(a, b) printf("%d %d\n", a, b)
+#define st(v) sort(v.begin(), v.end())
+#define pr pair
+#define pb push_back
+#define f(i, a, b) for (i = a; i < b; i++)
+
+string s;
+vector<int> v(9, 0);
+
+void init()
+{
+    cin >> s;
+}
+
+void solve()
+{
+    int i;
+    f(i, 0, s.length())
+    {
+        if (s[i] == '9')
+        {
+            v[6]++;
+        }
+        else
+        {
+            v[s[i] - '0']++;
+        }
+    }
+
+    v[6] = (v[6] + 1) / 2;
+
+    pnd1(*max_element(v.begin(), v.end()));
+}
+
+int main(void)
+{
+    fio;
+    init();
+    solve();
+    return 0;
+}
