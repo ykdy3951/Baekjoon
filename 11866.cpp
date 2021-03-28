7dbe2ce9ec18ca6bdc391c18726a9b7d4c7b2885
Chanwoong Kim <ykdy3951@naver.com>
2021-03-28 20:41:28 +0900

    boj 11866

---
+++ 11866.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define endl '\n'
+#define ll long long
+#define INF 987654321
+#define sc1(a) scanf("%c", &a)
+#define ss1(a) scanf("%s", s)
+#define sd1(a) scanf("%d", &a)
+#define sd2(a, b) scanf("%d %d", &a, &b)
+#define sd3(a, b, c) scanf("%d %d %d", &a, &b, &c)
+#define sd4(a, b, c, d) scanf("%d %d %d %d", &a, &b, &c, &d)
+#define sld1(a) scanf("%ld", &a)
+#define sld2(a, b) scanf("%ld %ld", &a, &b)
+#define slld1(a) scanf("%lld", &a)
+#define slld2(a, b) scanf("%lld %lld", &a, &b)
+#define pd1(a) printf("%d", a)
+#define psd1(a) printf("%d ", a)
+#define pnd1(a) printf("%d\n", a)
+#define plld1(a) printf("%lld", a)
+#define pslld1(a) printf("%lld ", a)
+#define pnlld1(a) printf("%lld\n", a)
+#define pd2(a, b) printf("%d %d", a, b)
+#define pnd2(a, b) printf("%d %d\n", a, b)
+#define st(v) sort(v.begin(), v.end())
+#define vt vector
+#define pr pair
+#define pb push_back
+#define f(i, a, b) for (int i = a; i < b; i++)
+
+int n, k;
+queue<int> q;
+
+void init()
+{
+    sd2(n, k);
+
+    f(i, 1, n + 1)
+    {
+        q.push(i);
+    }
+}
+
+void solve()
+{
+    printf("<");
+    while (!q.empty())
+    {
+        f(i, 0, k - 1)
+        {
+            int nu = q.front();
+            q.pop();
+            q.push(nu);
+        }
+        int nu = q.front();
+        q.pop();
+        if (!q.empty())
+        {
+            printf("%d, ", nu);
+        }
+
+        else
+            printf("%d>\n", nu);
+    }
+}
+
+int main(void)
+{
+    init();
+    solve();
+    return 0;
+}
