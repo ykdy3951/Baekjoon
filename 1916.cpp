9e0590f23790c6f435ec3e0485dd6c802a817345
Chanwoong Kim <ykdy3951@naver.com>
2021-04-14 04:25:46 +0900

    boj 1916

--- 1916.cpp
+++ 1916.cpp
@@ -2,61 +2,92 @@
 
 using namespace std;
 
-#define MAX 1001
-#define INF_ 987654321
+#define fio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define endl '\n'
+#define ll long long
+#define INF 987654321
+#define sc1(a) scanf("%c", &a)
+#define ss1(a) scanf("%s", s)
+#define sd1(a) scanf("%d", &a)
+#define sd2(a, b) scanf("%d %d", &a, &b)
+#define sd3(a, b, c) scanf("%d %d %d", &a, &b, &c)
+#define sd4(a, b, c, d) scanf("%d %d %d %d", &a, &b, &c, &d)
+#define sld1(a) scanf("%ld", &a)
+#define sld2(a, b) scanf("%ld %ld", &a, &b)
+#define slld1(a) scanf("%lld", &a)
+#define slld2(a, b) scanf("%lld %lld", &a, &b)
+#define pd1(a) printf("%d", a)
+#define psd1(a) printf("%d ", a)
+#define pnd1(a) printf("%d\n", a)
+#define plld1(a) printf("%lld", a)
+#define pslld1(a) printf("%lld ", a)
+#define pnlld1(a) printf("%lld\n", a)
+#define pd2(a, b) printf("%d %d", a, b)
+#define pnd2(a, b) printf("%d %d\n", a, b)
+#define st(v) sort(v.begin(), v.end())
+#define vt vector
+#define pr pair
+#define pb push_back
+#define f(i, a, b) for (int i = a; i < b; i++)
+#define fe(i, a, b) for (int i = a; i <= b; i++)
 
-priority_queue<pair<int, int>> pq;
-vector<pair<int, int>> graph[MAX];
-int ans[MAX];
-int n, m;
-int s, e;
-
-int dijkstra()
-{
-    pq.push({0, s});
-    while (!pq.empty())
-    {
-        int now = pq.top().second;
-        int weight = -pq.top().first;
-        if (now == e)
-            return weight;
-        pq.pop();
-        for (auto next : graph[now])
-        {
-            if (ans[next.first] >= weight + next.second)
-            {
-                ans[next.first] = weight + next.second;
-                pq.push({-ans[next.first], next.first});
-            }
-        }
-    }
-}
+int n, m, s, e;
+vt<pr<int, int>> G[1001];
+bool chk[1001];
+int dist[1001];
+priority_queue<pr<int, int>, vt<pr<int, int>>, greater<pr<int, int>>> pq;
 
 void init()
 {
-    scanf("%d", &n);
-    scanf("%d", &m);
-
-    for (int i = 1; i <= m; i++)
+    sd2(n, m);
+    f(i, 0, m)
     {
-        int u, v, w;
-        scanf("%d %d %d", &u, &v, &w);
-        graph[u].push_back({v, w});
+        int a, b, w;
+        sd3(a, b, w);
+
+        G[a].pb({b, w});
     }
 
-    for (int i = 1; i <= n; i++)
-        ans[i] = INF_;
+    fe(i, 1, n)
+    {
+        dist[i] = 1e9;
+    }
+    sd2(s, e);
+}
 
-    scanf("%d", &s);
-    scanf("%d", &e);
+void dijkstra()
+{
+    pq.push({0, s});
+    dist[s] = 0;
+    while (!pq.empty())
+    {
+        int w = pq.top().first, now = pq.top().second;
+        pq.pop();
 
-    ans[s] = 0;
+        if (now == e)
+        {
+            pnd1(w);
+            return;
+        }
+
+        for (int i = 0; i < G[now].size(); i++)
+        {
+            int nxt = G[now][i].first, dw = G[now][i].second;
+            if (chk[nxt])
+                continue;
+
+            if (dist[nxt] <= dist[now] + dw)
+                continue;
+
+            dist[nxt] = w + dw;
+            pq.push({w + dw, nxt});
+        }
+    }
 }
 
 int main(void)
 {
     init();
-    printf("%d", dijkstra());
-
+    dijkstra();
     return 0;
 }
