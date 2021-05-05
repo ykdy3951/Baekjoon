c77f789129cad90062ad52761baaa84bdf2e30a5
Chanwoong Kim <ykdy3951@naver.com>
2021-05-05 21:30:13 +0900

    boj 1655

---
+++ 1655.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define endl '\n'
+#define ll long long
+#define INF 987654321
+#define sc1(a) scanf("%c", &a)
+#define ss1(a) scanf("%s", s)
+#define sd1(a) scanf("%d", &a)
+#define sd2(a, b) scanf("%d %d", &a, &b)
+#define sd3(a, b, c) scanf("%d %d %d", &a, &b, &c)
+#define sd4(a, b, c, d) scanf("%d %d %d %d", &a, &b, &c, &d)
+#define sld1(a) scanf("%ld", &a)
+#define sld2(a, b) scanf("%ld %ld", &a, &b)
+#define slld1(a) scanf("%lld", &a)
+#define slld2(a, b) scanf("%lld %lld", &a, &b)
+#define pd1(a) printf("%d", a)
+#define psd1(a) printf("%d ", a)
+#define pnd1(a) printf("%d\n", a)
+#define plld1(a) printf("%lld", a)
+#define pslld1(a) printf("%lld ", a)
+#define pnlld1(a) printf("%lld\n", a)
+#define pd2(a, b) printf("%d %d", a, b)
+#define pnd2(a, b) printf("%d %d\n", a, b)
+#define st(v) sort(v.begin(), v.end())
+#define vt vector
+#define pr pair
+#define pb push_back
+#define f(i, a, b) for (int i = a; i < b; i++)
+#define fe(i, a, b) for (int i = a; i <= b; i++)
+
+int n;
+priority_queue<int> maxHeap;
+priority_queue<int, vt<int>, greater<int>> minHeap;
+
+void solve()
+{
+	sd1(n);
+	f(i, 0, n)
+	{
+		int num;
+		sd1(num);
+		if (maxHeap.size() == 0)
+		{
+			maxHeap.push(num);
+		}
+		else
+		{
+			if (maxHeap.size() <= minHeap.size())
+			{
+				maxHeap.push(num);
+			}
+
+			else
+			{
+				minHeap.push(num);
+			}
+
+			if (maxHeap.top() > minHeap.top())
+			{
+				int maxTop = maxHeap.top();
+				int minTop = minHeap.top();
+				maxHeap.pop();
+				minHeap.pop();
+				maxHeap.push(minTop);
+				minHeap.push(maxTop);
+			}
+		}
+
+		pnd1(maxHeap.top());
+	}
+}
+
+int main(void)
+{
+	solve();
+	return 0;
+}
