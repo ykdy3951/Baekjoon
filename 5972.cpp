07809de88396bb3fbc8e4151d803b79dfc233c60
Chanwoong Kim <ykdy3951@naver.com>
2022-06-01 19:59:49 +0900

    boj 5972

---
+++ 5972.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define endl '\n'
+#define ll long long
+#define INF 987654321
+#define sc1(a) scanf("%c", &a)
+#define ss1(a) scanf("%s", s)
+#define sd1(a) scanf("%d", &a)
+#define sd2(a, b) scanf("%d %d", &a, &b)
+#define sd3(a, b, c) scanf("%d %d %d", &a, &b, &c)
+#define sd4(a, b, c, d) scanf("%d %d %d %d", &a, &b, &c, &d)
+#define sld1(a) scanf("%ld", &a)
+#define sld2(a, b) scanf("%ld %ld", &a, &b)
+#define slld1(a) scanf("%lld", &a)
+#define slld2(a, b) scanf("%lld %lld", &a, &b)
+#define pd1(a) printf("%d", a)
+#define psd1(a) printf("%d ", a)
+#define pnd1(a) printf("%d\n", a)
+#define plld1(a) printf("%lld", a)
+#define pslld1(a) printf("%lld ", a)
+#define pnlld1(a) printf("%lld\n", a)
+#define pd2(a, b) printf("%d %d", a, b)
+#define pnd2(a, b) printf("%d %d\n", a, b)
+#define st(v) sort(v.begin(), v.end())
+#define vt vector
+#define pr pair
+#define pb push_back
+#define f(i, a, b) for (int i = a; i < b; i++)
+#define fe(i, a, b) for (int i = a; i <= b; i++)
+
+int n, m, ans;
+vt<pr<int, int>> v[50005];
+priority_queue<pr<int, int>> pq;
+int cost[50005];
+
+void init()
+{
+    sd2(n,m);
+    f(i, 0, m) {
+        int a, b, c;
+        sd3(a,b,c);
+        v[a].pb({b, c});
+        v[b].pb({a, c});
+    }
+
+    // 초기화
+    fe(i, 1, n) {
+        cost[i] = INF;
+    }
+
+
+    pq.push({0, 1});
+}
+
+void solve()
+{
+    while(!pq.empty()) 
+    {
+        int w = -pq.top().first, now = pq.top().second;
+        pq.pop();
+        
+        if (now == n) {
+            ans=w;
+            return;
+        }
+
+        for(pr<int, int> p : v[now]) {
+
+            int next = p.first, nw = p.second + w;
+
+            if(nw >= cost[next]) continue; 
+
+            cost[next] = nw;
+            pq.push({-nw, next});
+        }
+    }
+}
+
+int main(void)
+{
+    init();
+    solve();
+    pnd1(ans);
+    return 0;
+}
