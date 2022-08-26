98a615e69aa03331bd52c0a6a726676b3e8b68d6
ykdy3951 <ykdy3951@naver.com>
2022-08-26 20:44:35 +0900

    boj 10211

---
+++ 10211.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+#define fio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+#define endl '\n'
+#define ll long long
+#define INF 987654321
+#define sc1(a) scanf("%c", &a)
+#define ss1(a) scanf("%s", s)
+#define sd1(a) scanf("%d", &a)
+#define sd2(a, b) scanf("%d %d", &a, &b)
+#define sd3(a, b, c) scanf("%d %d %d", &a, &b, &c)
+#define sd4(a, b, c, d) scanf("%d %d %d %d", &a, &b, &c, &d)
+#define sld1(a) scanf("%ld", &a)
+#define sld2(a, b) scanf("%ld %ld", &a, &b)
+#define slld1(a) scanf("%lld", &a)
+#define slld2(a, b) scanf("%lld %lld", &a, &b)
+#define pd1(a) printf("%d", a)
+#define psd1(a) printf("%d ", a)
+#define pnd1(a) printf("%d\n", a)
+#define plld1(a) printf("%lld", a)
+#define pslld1(a) printf("%lld ", a)
+#define pnlld1(a) printf("%lld\n", a)
+#define pd2(a, b) printf("%d %d", a, b)
+#define pnd2(a, b) printf("%d %d\n", a, b)
+#define st(v) sort(v.begin(), v.end())
+#define vt vector
+#define pr pair
+#define pb push_back
+#define f(i, a, b) for (int i = a; i < b; i++)
+#define fe(i, a, b) for (int i = a; i <= b; i++)
+
+int t;
+int n;
+int arr[1005], dp[1005];
+
+void solve() 
+{
+    f(i,1,n)
+    {
+        dp[i]=max(arr[i], dp[i-1]+arr[i]);
+    }
+    pnd1(*max_element(dp,dp+n));
+}   
+
+void init()
+{
+    sd1(t);
+    while(t--)
+    {
+        sd1(n);
+        f(i,0,n)
+        {
+            sd1(arr[i]);
+        }
+        dp[0]=arr[0];
+
+        solve();
+    }
+}
+
+int main(void)
+{
+    fio;
+    init();
+    return 0;
+}
