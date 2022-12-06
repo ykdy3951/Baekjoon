be749d1a9fdbac2bf2b41c1eb5602c3c4e0ffbb6
Chanwoong Kim <ykdy3951@naver.com>
2022-12-06 14:36:29 +0900

    boj 13194

---
+++ 13194.py
@@ -0,0 +1,7 @@
+print(312500,7500)
+for i in range(5):
+    for j in range(250):
+        print(i*250+j+1, 2*1250+i+1)
+for i in range(5):
+    for j in range(1250):
+        print(2*1250+i+1,j+1+1250)
