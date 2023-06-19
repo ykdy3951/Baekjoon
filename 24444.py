a27a700c29a258c74b8bca6d8cc6a28943e4f544
ykdy3951 <ykdy3951@naver.com>
2023-06-19 23:32:07 +0900

    boj 24444

--- 24444.py
+++ 24444.py
@@ -5,18 +5,17 @@ for _ in range(m):
     x,y=map(int,input().split())
     g[x].append(y)
     g[y].append(x)
-for i in range(1,n+1):
-    g[i].sort()
 d=deque([r])
 cnt = 1
 chk=[0]*(n+1)
+chk[r]=1
 while d:
     a=d.popleft()
-    chk[a]=cnt
-    cnt += 1
+    g[a].sort()
     for i in g[a]:
         if not chk[i]:
-            chk[i]=1
+            cnt += 1
+            chk[i]=cnt
             d.append(i)
 for i in range(1, n+1):
     print(chk[i])
