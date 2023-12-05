bb2acb73ea17f37ff14f9c50d6d65edec50d1978
Chanwoong Kim <ykdy3951@naver.com>
2023-12-05 14:48:05 +0900

    boj 5497

--- 5497.py
+++ 5497.py
@@ -4,14 +4,6 @@ ans=[]
 for i in range(n):
     for j in range(n):
         if a[i][j]==1:
-            
-            #  Find the horizontally maximal black run from i. = k
-            #  Assume pixels in (i,j) to (i,k-1) are all black.
-            #  Find the vertically and maximal black run from i. = l
-            #  Assume pixels in (i,j) to (j,l-1) are all black.
-            #  // so rectangle region [i,k-1] × [j,l-1] is a black rectangle
-            #  XOR (j, k-1, i, l-1);
-
             k = j
             while k < n and a[i][k] == 1:
                 k += 1
