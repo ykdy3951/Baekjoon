8b66ab39adbb912df4898f2efce94a604f5c8054
Chanwoong Kim <ykdy3951@naver.com>
2023-12-19 17:35:09 +0900

    [FIX] run.sh

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ elif [ $extension = "c" ]; then
     gcc $file -o Main -O2 -Wall -lm -static -std=gnu11 -DONLINE_JUDGE -DBOJ && ./Main && rm Main
 # python file
 elif [ $extension = "py" ]; then
-    python -W ignore -c "import py_compile; pycompile.compile(r'$file')" && python3 -W ignore $file
+    python3 -W ignore -c "import py_compile; py_compile.compile(r'$file')" && python3 -W ignore $file
 # sv file
 elif [ $extension = "sv" ]; then
     iverilog -g2012 -o Main $file && vvp Main && rm Main
